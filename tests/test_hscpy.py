d392e0491c11fd205d8ac936228ec0572f2c5dca
Francesco Terenzi <fra.terenz1993@gmail.com>
2023-09-15 11:36:10 +0100

    update tests

--- tests/test_hscpy.py
+++ tests/test_hscpy.py
@@ -2,13 +2,17 @@ import pytest
 from hscpy import get_idx_timepoint_from_age
 
 
+def test_get_idx_timepoint_from_age_simple_21():
+    assert get_idx_timepoint_from_age(40, 80, 21, False) == (11, 40.0)
+
+
 def test_get_idx_timepoint_from_age_simple():
-    assert get_idx_timepoint_from_age(41, 81, 4) == (3, 28.0)
+    assert get_idx_timepoint_from_age(40, 80, 5, False) == (3, 40.0)
 
 
-def test_get_idx_timepoint_from_age_float():
-    assert get_idx_timepoint_from_age(41, 81, 19) == (10, 41.5)
+def test_get_idx_timepoint_from_age_not_matching_lower_21():
+    assert get_idx_timepoint_from_age(41, 80, 21, False) == (11, 40.0)
 
 
-def test_get_idx_timepoint_from_age():
-    assert get_idx_timepoint_from_age(48, 81, 19) == (9, 46)
+def test_get_idx_timepoint_from_age_not_matching_21():
+    assert get_idx_timepoint_from_age(43, 80, 21, False) == (10, 44.0)
