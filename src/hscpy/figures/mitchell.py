50d53a2ffd4fc71c6820140eb386f0bdce951c48
Francesco Terenzi <fra.terenz1993@gmail.com>
2023-10-09 14:01:35 +0100

    load_and_process mitchell's data

--- src/hscpy/figures/mitchell.py
+++ src/hscpy/figures/mitchell.py
@@ -1,7 +1,10 @@
 """Load patient's data"""
 import pandas as pd
 from pathlib import Path
-from typing import Tuple
+from typing import NewType, Tuple
+
+
+Mitchell = NewType("Mitchell", pd.DataFrame)
 
 
 def load_patient(
@@ -18,3 +21,27 @@ def load_patient(
 
 def filter_mutations(m_matrix: pd.DataFrame, m_type: pd.DataFrame) -> pd.DataFrame:
     return m_matrix.iloc[m_type[m_type == "SNV"].dropna().index, :]
+
+
+def load_and_process_mitchell(path2sims: Path) -> Mitchell:
+    assert (
+        path2sims.name == "Summary_cut.csv"
+    ), f"wrong arg `path2sims` {path2sims}, should be the path to the file `Summary_cut.csv`"
+    summary = pd.read_csv(path2sims, index_col=0)
+    summary.cell_type = summary.cell_type.astype("category")
+    summary.sample_type = summary.sample_type.astype("category")
+    summary.sort_values(by="age", inplace=True)
+    summary.reset_index(inplace=True)
+
+    # neglect some duplicated colonies e.g. summary.colony_ID == "11_E07"
+    summary = summary.merge(
+        summary[["donor_id", "age"]]
+        .groupby("donor_id")
+        .count()
+        .reset_index()
+        .rename(columns={"age": "cells"}),
+        on="donor_id",
+        validate="many_to_one",
+        how="left",
+    )
+    return Mitchell(summary)
