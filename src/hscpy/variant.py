7ea88b34c1d72874a329c79151ee790fecc12ab8
Francesco Terenzi <fra.terenz1993@gmail.com>
2023-10-23 14:32:31 +0100

    load variant counts (nb of clones) by age

--- src/hscpy/variant.py
+++ src/hscpy/variant.py
@@ -6,6 +6,33 @@ from hscpy import load_variant, parse_path2folder_xdoty_years
 from hscpy.parameters import parameters_from_path
 
 
+class RealisationVariantCounts:
+    def __init__(self, path: Path) -> None:
+        assert path.is_file(), f"cannot find file {path}"
+        self.parameters = parameters_from_path(path)
+        self.variant_counts = len([ele for ele in load_variant(path) if ele > 0])
+
+
+def load_all_var_counts_by_age(
+    path2dir: Path,
+) -> Dict[float, List[RealisationVariantCounts]]:
+    assert path2dir.is_dir()
+    var_counts_sims = dict()
+
+    for path in path2dir.iterdir():
+        i = 0
+        if path.is_dir():
+            var_counts_sims[parse_path2folder_xdoty_years(path)] = list()
+            for i, p in enumerate(path.glob("*.csv")):
+                var_counts_sims[parse_path2folder_xdoty_years(p.parent)].append(
+                    RealisationVariantCounts(p)
+                )
+
+            print(f"loaded {i + 1} files from {path}")
+
+    return var_counts_sims
+
+
 class RealisationVariantFractions:
     def __init__(self, path: Path) -> None:
         assert path.is_file(), f"cannot find file {path}"
