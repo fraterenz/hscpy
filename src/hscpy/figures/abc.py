159b83c028adcaa35e93d110841b577537916aca
Francesco Terenzi <hfx923@qmul.ac.uk>
2024-02-02 14:48:59 +0000

    nicer plots for the posteriors with grids

--- src/hscpy/figures/abc.py
+++ src/hscpy/figures/abc.py
@@ -3,15 +3,154 @@ from pathlib import Path
 from typing import Dict, List, Tuple
 from futils import snapshot
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+from matplotlib import ticker
 import pandas as pd
 import seaborn as sns
 import numpy as np
+from scipy import stats
 
 from hscpy import abc, realisation
 from hscpy.figures import AgeSims
 
+
+class Gamma:
+    def __init__(self, mean, std):
+        self.mean = mean
+        self.std = std
+
+    def shape_scale_from_mean_std(self):
+        return self.mean**2 / self.std**2, self.std**2 / self.mean
+
+    def plot(self, ax, **kwargs):
+        shape, scale = self.shape_scale_from_mean_std()
+        x = np.linspace(
+            stats.gamma.ppf(0.00001, shape, scale=scale),
+            stats.gamma.ppf(0.99999, shape, scale=scale),
+            100,
+        )
+        ax.plot(x, stats.gamma.pdf(x, shape, 0, scale), **kwargs)
+        return ax
+
+
+def plot_posteriors_fancy(
+    accepted: pd.Series,
+    xlabel: str,
+    bins,
+    ax,
+    color,
+    fancy: bool,
+    legend: bool = False,
+):
+    # https://matplotlib.org/stable/gallery/lines_bars_and_markers/stairs_demo.html
+    bin_distance = (bins[1] - bins[0]) / 2
+    if fancy:
+        values, _ = np.histogram(
+            accepted, bins=bins, density=True
+        )  # TODO density=True is ok?
+        ax.fill_between(
+            bins[:-1] + bin_distance, values, ls="-", color=color, alpha=0.15
+        )
+        ax.plot(
+            bins[:-1] + bin_distance,
+            values,
+            ls="-",
+            marker=".",
+            mew=3,
+            color=color,
+            alpha=0.5,
+        )
+    else:
+        values, bins, _ = ax.hist(
+            accepted,
+            align="mid",
+            alpha=0.4,
+            density=True,
+            bins=bins,
+            edgecolor="black",
+            color=color,
+        )
+
+    # MAP
+    point_estimate = bins[np.argmax(values)] + bin_distance
+    ax.axvline(x=point_estimate, mew=3, ls="--", c=color)
+    ax.set_xlim([bins[0] - bin_distance, bins[-1] + bin_distance])
+
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel("pdf")
+    if legend:
+        ax.legend()
+    return point_estimate
+
+
+def fmt_two_digits(x, pos):
+    return f"{x:.2f}"
+
+
+def plot_posteriors_grid_eta_sigma_tau_mu(
+    posterior: pd.DataFrame,
+    name: str,
+    fig,
+    color: str,
+    bins_eta,
+    bins_sigma,
+    bins_tau,
+    bins_mu,
+    fancy: bool = True,
+):
+    # posteriors
+    point_estimate_eta = plot_posteriors_fancy(
+        posterior.eta, r"$\eta$", bins_eta, fig.axes[2], color, fancy=fancy
+    )
+    point_estimate_sigma = plot_posteriors_fancy(
+        posterior.sigma, r"$\sigma$", bins_sigma, fig.axes[3], color, fancy=fancy
+    )
+    _ = plot_posteriors_fancy(
+        posterior.tau, r"$\tau$", bins_tau, fig.axes[4], color, fancy=fancy
+    )
+    _ = plot_posteriors_fancy(
+        posterior.mu, r"$\mu$", bins_mu, fig.axes[5], color, fancy=fancy
+    )
+
+    fig.axes[3].set_ylabel("")
+    fig.axes[5].set_ylabel("")
+
+    # gamma
+    gamma = Gamma(point_estimate_eta, point_estimate_sigma)
+    gamma.plot(fig.axes[0], label=name, color=color)
+
+    return fig, gamma
+
+
+def create_posteriors_grid_eta_sigma_tau_mu():
+    fig = plt.figure(figsize=[8, 7.5], layout="constrained")
+    # init axes
+    gs0 = gridspec.GridSpec(2, 1, figure=fig, height_ratios=[1, 2])
+    gs00 = gridspec.GridSpecFromSubplotSpec(1, 4, subplot_spec=gs0[0])
+    gs01 = gs0[1].subgridspec(2, 2)
+    ax1 = fig.add_subplot(gs00[0, :-1])
+    # ax1.set_ylim([-1, 22])
+    ax1.set_ylabel("pdf")
+    ax1.set_xlabel(r"$s$")  # TODO
+    ax2 = fig.add_subplot(gs00[0, -1])
+    ax2.axis("off")
+    ax3 = fig.add_subplot(gs01[0, 0])
+    ax4 = fig.add_subplot(gs01[0, 1])
+    ax4.xaxis.set_major_formatter(ticker.FuncFormatter(fmt_two_digits))
+    # ax4.yaxis.set_major_formatter(ticker.FuncFormatter(fmt_two_digits))
+    ax5 = fig.add_subplot(gs01[1, 0])
+    ax6 = fig.add_subplot(gs01[1, 1])
+
+    return fig
+
+
 class SyntheticValidation:
-    def __init__( self, idx: int, sims_sfs: Dict[AgeSims, List[realisation.RealisationSfs]], sims_clones: pd.DataFrame):
+    def __init__(
+        self,
+        idx: int,
+        sims_sfs: Dict[AgeSims, List[realisation.RealisationSfs]],
+        sims_clones: pd.DataFrame,
+    ):
         self.target_sfs = {k: sims_sfs[k][idx] for k in sims_sfs.keys()}
         self.params = self.target_sfs[AgeSims(0.0)].parameters.into_dict()
 
@@ -20,7 +159,8 @@ class SyntheticValidation:
         )
 
         target_clones = sims_clones.loc[
-            sims_clones.idx == self.params["idx"], ["age", "variant counts detected", "mu"]
+            sims_clones.idx == self.params["idx"],
+            ["age", "variant counts detected", "mu"],
         ].rename(columns={"variant counts detected": "target clones detected"})
         assert (self.params["mu"] == target_clones.mu).all()
         target_clones.drop(columns=["mu"], inplace=True)
@@ -40,57 +180,68 @@ class SyntheticValidation:
         bins_mu: np.ndarray = np.arange(0, 20, 1),
         bins_tau: np.ndarray = np.arange(0, 5.2, 0.2),
         bins_std: np.ndarray = np.arange(0, 0.12, 0.01),
-        density: bool = False
+        density: bool = False,
     ):
-    
         runs2keep = abc.run_abc_sfs_clones(
             self.abc, quantile_sfs, quantile_clones, proprtion_runs_disc
         )
-    
-        view_synthetic = self.abc[self.abc.idx.isin(runs2keep)].drop_duplicates(subset="idx")
+
+        view_synthetic = self.abc[self.abc.idx.isin(runs2keep)].drop_duplicates(
+            subset="idx"
+        )
         assert not view_synthetic.empty, "empty posterior"
-    
+
         print(f"ABC combined kept {len(runs2keep)} runs")
-    
+
         gs = []
-    
+
         axd = plot_results(view_synthetic, "s", "mu", bins_s, bins_mu, density=density)
         axd["C"].axvline(self.params["s"])
         axd["C"].axhline(self.params["mu"])
         gs.append(axd)
         plt.show()
-    
-        axd = plot_results(view_synthetic, "s", "std", bins_s, bins_std, density=density)
+
+        axd = plot_results(
+            view_synthetic, "s", "std", bins_s, bins_std, density=density
+        )
         axd["C"].axvline(self.params["s"])
         axd["C"].axhline(self.params["std"])
         gs.append(axd)
         plt.show()
-    
-        axd = plot_results(view_synthetic, "s", "tau", bins_s, bins_tau, density=density)
+
+        axd = plot_results(
+            view_synthetic, "s", "tau", bins_s, bins_tau, density=density
+        )
         axd["C"].axvline(self.params["s"])
         axd["C"].axhline(self.params["tau"])
         gs.append(axd)
         plt.show()
-    
-        axd = plot_results(view_synthetic, "mu", "tau", bins_mu, bins_tau, density=density)
+
+        axd = plot_results(
+            view_synthetic, "mu", "tau", bins_mu, bins_tau, density=density
+        )
         axd["C"].axvline(self.params["mu"])
         axd["C"].axhline(self.params["tau"])
         gs.append(axd)
         plt.show()
-    
-        axd = plot_results(view_synthetic, "mu", "std", bins_mu, bins_std, density=density)
+
+        axd = plot_results(
+            view_synthetic, "mu", "std", bins_mu, bins_std, density=density
+        )
         axd["C"].axvline(self.params["mu"])
         axd["C"].axhline(self.params["std"])
         gs.append(axd)
         plt.show()
-    
-        axd = plot_results(view_synthetic, "tau", "std", bins_tau, bins_std, density=density)
+
+        axd = plot_results(
+            view_synthetic, "tau", "std", bins_tau, bins_std, density=density
+        )
         axd["C"].axvline(self.params["tau"])
         axd["C"].axhline(self.params["std"])
         gs.append(axd)
-    
+
         plt.show()
-    
+
         return gs
 
 
@@ -106,18 +257,23 @@ def plot_prior(prior: pd.Series, ax, **kwargs):
 
 
 def plot_results(
-    results: pd.DataFrame, x: str, y: str, xbins: np.ndarray, ybins: np.ndarray, density: bool = False
+    results: pd.DataFrame,
+    x: str,
+    y: str,
+    xbins: np.ndarray,
+    ybins: np.ndarray,
+    density: bool = False,
 ) -> Dict:
     mapping = {
         "tau": r"$\tau$",
         "mu": r"$\mu$",
-        "std": r"$\sigma$",
-        "s": r"$s$",
-        "s_tau": r"$s / \tau$",
-        "std_tau": r"$\sigma / \tau$",
+        "eta": r"$\eta$",
+        "sigma": r"$\sigma$",
     }
     if density:
-        print("WARNING setting `density=True` is buggy: the yaxis of the top plot seems wrong")
+        print(
+            "WARNING setting `density=True` is buggy: the yaxis of the top plot seems wrong"
+        )
 
     xlims = [xbins.min() - (xbins[1] - xbins[0]), xbins.max() + (xbins[1] - xbins[0])]
     ylims = [ybins.min() - (ybins[1] - ybins[0]), ybins.max() + (ybins[1] - ybins[0])]
@@ -132,19 +288,33 @@ def plot_results(
         # set the width ratios between the columns
         width_ratios=[3.5, 1],
         per_subplot_kw={
-            "A": {"xticklabels": [], "xlim": xlims, "ylabel": "pdf" if density else "counts"},
+            "A": {
+                "xticklabels": [],
+                "xlim": xlims,
+                "ylabel": "pdf" if density else "counts",
+            },
             "C": {
                 "xlim": xlims,
                 "ylim": ylims,
                 "xlabel": mapping.get(x, x),
                 "ylabel": mapping.get(y, x),
             },
-            "D": {"yticklabels": [], "ylim": ylims, "xlabel": "pdf" if density else "counts"},
+            "D": {
+                "yticklabels": [],
+                "ylim": ylims,
+                "xlabel": "pdf" if density else "counts",
+            },
         },
     )
 
     axd["A"].hist(results[x], density=density, bins=xbins, edgecolor="black")
-    axd["D"].hist(results[y], bins=ybins, density=density, orientation="horizontal", edgecolor="black")
+    axd["D"].hist(
+        results[y],
+        bins=ybins,
+        density=density,
+        orientation="horizontal",
+        edgecolor="black",
+    )
     axd["C"].hist2d(x=results[x], y=results[y], bins=[xbins, ybins], cmap="Greys")
 
     # force lims after hist2d plot
